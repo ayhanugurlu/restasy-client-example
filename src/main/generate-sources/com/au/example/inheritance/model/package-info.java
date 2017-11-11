c30502c4c6c3b048e192b795ad99dddffb417d60
ayhanu <ayhanugurlu@aka-soft.com>
2017-11-11 19:48:29 +0300

    remove generated source

--- src/main/generate-sources/com/au/example/inheritance/model/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://model.inheritance.example.au.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.au.example.inheritance.model;
